bf649fffa77df84330dffc036320dd82fafaa886
dotnetspec <prmockridge@gmail.com>
2019-03-21 19:08:06 +0800

    Add a rule to process URL paths with . (dot). Completed (but not fully working - still 'nameless')

--- publik/src/components/Profile.jsx
+++ publik/src/components/Profile.jsx
@@ -63,21 +63,56 @@ export default class Profile extends Component {
 
  fetchData() {
    this.setState({ isLoading: true })
-   const options = { decrypt: false }
-   getFile('statuses.json', options)
-     .then((file) => {
-       var statuses = JSON.parse(file || '[]')
-       this.setState({
-         person: new Person(loadUserData().profile),
-         username: loadUserData().username,
-         statusIndex: statuses.length,
-         statuses: statuses,
+   if (this.isLocal()) {
+     const options = { decrypt: false }
+     getFile('statuses.json', options)
+       .then((file) => {
+         var statuses = JSON.parse(file || '[]')
+         this.setState({
+           person: new Person(loadUserData().profile),
+           username: loadUserData().username,
+           statusIndex: statuses.length,
+           statuses: statuses,
+         })
        })
-     })
-     .finally(() => {
-       this.setState({ isLoading: false })
-     })
+       .finally(() => {
+         this.setState({ isLoading: false })
+       })
+   } else {
+     const username = this.props.match.params.username
+
+     lookupProfile(username)
+       .then((profile) => {
+         this.setState({
+           person: new Person(profile),
+           username: username
+         })
+       })
+       .catch((error) => {
+         console.log('could not resolve profile')
+       })
+
+       const options = { username: username, decrypt: false }
+       getFile('statuses.json', options)
+         .then((file) => {
+           var statuses = JSON.parse(file || '[]')
+           this.setState({
+             statusIndex: statuses.length,
+             statuses: statuses
+           })
+         })
+         .catch((error) => {
+           console.log('could not fetch statuses')
+         })
+         .finally(() => {
+           this.setState({ isLoading: false })
+         })
+   }
  }
+
+ isLocal() {
+  return this.props.match.params.username ? false : true
+}
 
  componentWillMount() {
     this.setState({
@@ -90,67 +125,69 @@ export default class Profile extends Component {
    this.fetchData()
  }
 
-   render() {
-    const { handleSignOut } = this.props;
-    const { person } = this.state;
-    const { username } = this.state;
+ render() {
+      const { handleSignOut } = this.props;
+      const { person } = this.state;
+      const { username } = this.state;
 
-    return (
-      !isSignInPending() && person ?
-      <div className="container">
-        <div className="row">
-          <div className="col-md-offset-3 col-md-6">
-            <div className="col-md-12">
-              <div className="avatar-section">
-                <img
-                  src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage }
-                  className="img-rounded avatar"
-                  id="avatar-image"
-                />
-                <div className="username">
-                  <h1>
-                    <span id="heading-name">{ person.name() ? person.name()
-                      : 'Nameless Person' }</span>
-                    </h1>
-                  <span>{username}</span>
-                  <span>
-                    &nbsp;|&nbsp;
-                    <a onClick={ handleSignOut.bind(this) }>(Logout)</a>
-                  </span>
-                </div>
-              </div>
-            </div>
-
-            <div className="new-status">
-            <div className="col-md-12 statuses">
-                {this.state.isLoading && <span>Loading...</span>}
-                {this.state.statuses.map((status) => (
-                    <div className="status" key={status.id}>
-                      {status.text}
-                    </div>
-                  )
-                )}
-              </div>
-              <div className="col-md-12">
-                <textarea className="input-status"
-                  value={this.state.newStatus}
-                  onChange={e => this.handleNewStatusChange(e)}
-                  placeholder="Enter a status"
-                />
-              </div>
-              <div className="col-md-12">
-                <button
-                  className="btn btn-primary btn-lg"
-                  onClick={e => this.handleNewStatusSubmit(e)}
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div> : null
-    );
-  }
+      return (
+       !isSignInPending() && person ?
+       <div className="container">
+         <div className="row">
+           <div className="col-md-offset-3 col-md-6">
+             <div className="col-md-12">
+               <div className="avatar-section">
+                 <img
+                   src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage }
+                   className="img-rounded avatar"
+                   id="avatar-image"
+                 />
+                 <div className="username">
+                   <h1>
+                     <span id="heading-name">{ person.name() ? person.name()
+                       : 'Nameless Person' }</span>
+                   </h1>
+                   <span>{username}</span>
+                   {this.isLocal() &&
+                     <span>
+                       &nbsp;|&nbsp;
+                       <a onClick={ handleSignOut.bind(this) }>(Logout)</a>
+                     </span>
+                   }
+                 </div>
+               </div>
+             </div>
+             {this.isLocal() &&
+               <div className="new-status">
+                 <div className="col-md-12">
+                   <textarea className="input-status"
+                     value={this.state.newStatus}
+                     onChange={e => this.handleNewStatusChange(e)}
+                     placeholder="What's on your mind?"
+                   />
+                 </div>
+                 <div className="col-md-12 text-right">
+                   <button
+                     className="btn btn-primary btn-lg"
+                     onClick={e => this.handleNewStatusSubmit(e)}
+                   >
+                     Submit
+                   </button>
+                 </div>
+               </div>
+             }
+             <div className="col-md-12 statuses">
+             {this.state.isLoading && <span>Loading...</span>}
+             {this.state.statuses.map((status) => (
+                 <div className="status" key={status.id}>
+                   {status.text}
+                 </div>
+                 )
+             )}
+             </div>
+           </div>
+         </div>
+       </div> : null
+      );
+      }
 }
